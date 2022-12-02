e13de75e5b1fc45160c026dd8d0d8709a92e6f3c
Oana Belu <93271350+OanaBelu@users.noreply.github.com>
2022-12-03 01:11:12 +0200

    Update print_people_in_space.py

--- print_people_in_space.py
+++ print_people_in_space.py
@@ -6,6 +6,11 @@ import requests
 response = requests.get('http://api.open-notify.org/astros.json')
 json = response.json()
 
-print('The people currently in space are: ')
+number = json.get("number")
+
+print ('\nHow many people are in space right now?')
+print('There are currently', number, 'people in space right now.')
+
+print('\nThe name of the people currently in space are:\n ')
 for person in json['people']:
     print(person['name'])
